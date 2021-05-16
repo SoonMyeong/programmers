8cf40a4231c9d137a72fa38ce9b11d59d97d2b32
soonmyeong <smkwon@neonexsoft.com>
2021-05-16 23:10:14 +0900

    Level3_단어변환 : 일단 문제적기만.. 풀이는ㄴ ㅐ 일로..

--- src/com/soon/world/level3/네트워크/Solution.java
+++ src/com/soon/world/level3/네트워크/Solution.java
@@ -4,7 +4,8 @@ package com.soon.world.level3.네트워크;
  * DFS 문제!
  * 인접행렬 방문에 관한 문제.
  * DFS 에 대해 다시 한번 생각해 볼 수 있는 계기가 되었다.
- *
+ * --> dfs 한번이 끝났다는건 연결된 네트워크 한 depth 가 끝났다는 의미기 때문에
+ * dfs 를 한 뒤에 answer 값을 +1 해 준다.
  */
 public class Solution {
     public int solution(int n, int[][] computers) {
---
+++ src/com/soon/world/level3/단어변환/Solution.java
@@ -0,0 +1,12 @@
+package com.soon.world.level3.단어변환;
+
+public class Solution {
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
