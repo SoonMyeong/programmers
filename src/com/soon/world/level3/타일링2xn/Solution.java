d8bca53ebf8069a6ee8ee191502d48f216eedb2e
soonmyeong <smkwon@neonexsoft.com>
2021-05-19 22:29:37 +0900

    Level3_2xn타일링 시작

---
+++ src/com/soon/world/level3/타일링2xn/Solution.java
@@ -0,0 +1,15 @@
+package com.soon.world.level3.타일링2xn;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int test = 4;
+        System.out.println(solution.solution(test));
+    }
+}
