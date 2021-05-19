7f7911cfffb4ec6bd4897e0415706d3b7583104d
soonmyeong <smkwon@neonexsoft.com>
2021-05-19 23:04:40 +0900

    Level3_2xn타일링

--- src/com/soon/world/level3/타일링2xn/Solution.java
+++ src/com/soon/world/level3/타일링2xn/Solution.java
@@ -1,10 +1,25 @@
 package com.soon.world.level3.타일링2xn;
 
+
+import java.util.Arrays;
+
+/*
+ * DP 문제 인거 같음
+ * 피보나치 수열 생각 해내는 게 어려움
+ * 자세한 설명 : https://deveric.tistory.com/61 에서 참고
+ */
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int[] dp = new int[n];
+        Arrays.fill(dp,0);
+        dp[0] = 1;
+        dp[1] = 2;
 
-        return answer;
+        for(int i=2; i<n; i++) {
+            int num = dp[i-1] + dp[i-2];
+            dp[i] = num %  1000000007 ;
+        }
+        return dp[n-1];
     }
 
     public static void main(String[] args) {
