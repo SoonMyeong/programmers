ec338c65993bef86b40fda50ae51f8869d7eb2a8
soonmyeong <smkwon@neonexsoft.com>
2021-05-21 23:10:55 +0900

    Level3_정수삼각형

---
+++ src/com/soon/world/level3/정수삼각형/Solution.java
@@ -0,0 +1,13 @@
+package com.soon.world.level3.정수삼각형;
+
+public class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] test = {{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
+        System.out.println(solution.solution(test));
+    }
+}
