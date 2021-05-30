05ecbf9dec6c7966c59cca4661dca36626ee44c6
soonmyeong <smkwon@neonexsoft.com>
2021-05-31 00:00:36 +0900

    leve2_다리지나는트럭 review setting

---
+++ src/com/soon/world/review/level2/다리를지나는트럭/Solution.java
@@ -0,0 +1,11 @@
+package com.soon.world.review.level2.다리를지나는트럭;
+
+public class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
