a28733ab87cb88b7f46481bd4ea75208040808ad
soonmyeong <smkwon@neonexsoft.com>
2021-06-01 00:15:21 +0900

    Level2_다리를지나는트럭 리뷰

--- src/com/soon/world/review/level2/다리를지나는트럭/Solution.java
+++ src/com/soon/world/review/level2/다리를지나는트럭/Solution.java
@@ -1,11 +1,51 @@
 package com.soon.world.review.level2.다리를지나는트럭;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 기존 truck 클래스 만들고 풀던 방법 에서
+ * c style 로 구현 해봄  (문제 풀이 이해를 위해..)
+ *
+ * 마지막 분기에서 queue 에 0 을 넣어주는 생각을 못함
+ */
 public class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        return answer;
+        int weightCount = 0;
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int truck : truck_weights) {
+            while(true) {
+                if (queue.isEmpty()) {
+                    weightCount += truck;
+                    queue.add(truck);
+                    answer++;
+                    break;
+                } else if (queue.size() == bridge_length) {
+                    weightCount -= queue.poll();
+                } else {
+                    if(weightCount + truck <= weight) {
+                        weightCount += truck;
+                        queue.add(truck);
+                        answer++;
+                        break;
+                    }else {
+                        queue.add(0);
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer+bridge_length;
     }
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
 
+        System.out.println(solution.solution(bridge_length,weight,truck_weights));
     }
 }
