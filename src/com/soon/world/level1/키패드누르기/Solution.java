a60de45b85349255d6f988c2ef36c679e87e3f26
SOONMYEONGKWON <smkwon@neonexsoft.com>
2021-06-21 16:48:18 +0900

    키패드누르기 from bfs (main 수정)

--- src/com/soon/world/level1/키패드누르기/Solution.java
+++ src/com/soon/world/level1/키패드누르기/Solution.java
@@ -115,7 +115,7 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        Solution solution = new Test5();
+        Solution solution = new Solution();
         int [] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
         String hand  = "right";
         System.out.println(solution.solution(numbers,hand));
