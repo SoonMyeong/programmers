9aa38a2521c75c0083e93b538440166ddbdab9ec
SoonMyeong_notebook <soon_myeong@naver.com>
2021-08-21 18:19:52 +0900

    거리두기 확인하기

---
+++ src/com/soon/world/level2/거리두기확인하기/Solution.java
@@ -0,0 +1,13 @@
+package com.soon.world.level2.거리두기확인하기;
+
+public class Solution {
+
+    public int[] solution(String[][] places) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
