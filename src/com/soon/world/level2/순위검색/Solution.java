b8dd9b6e58121df5533de9f266ec01067f44dbf4
soonmyeong <smkwon@neonexsoft.com>
2021-08-29 23:12:28 +0900

    순위검색

---
+++ src/com/soon/world/level2/순위검색/Solution.java
@@ -0,0 +1,13 @@
+package com.soon.world.level2.순위검색;
+
+public class Solution {
+
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
