36c0b04d067d716fa80eb162da08faa29a51f298
soonmyeong <smkwon@neonexsoft.com>
2021-10-12 23:57:15 +0900

    ...ㅜㅠㅠ

--- src/com/soon/world/level2/위클리챌린지/전력망을_둘로_나누기/Solution.java
+++ src/com/soon/world/level2/위클리챌린지/전력망을_둘로_나누기/Solution.java
@@ -1,78 +1,52 @@
 package com.soon.world.level2.위클리챌린지.전력망을_둘로_나누기;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Solution {
-
-    //흠.. 인접그래프 구현해서 하면 될거같았는데...안된다....
-
-    class Node {
-        int num;
-        Node link;
-    }
-
-    class NodeList{
-        Node[] list = new Node[200];
-        List<Integer> result = new ArrayList<>();
-        int sum = 0;
-
-        void connect(int v1, int v2) {
-            Node node = new Node();
-            node.num = v2;
-            for(Node n : list) {
-                if(n!=null && n.num == v1) {
-                    n.link = node;
-                    return ;
-                }
-            }
-            node.link = list[v1];
-            list[v1] = node;
-        }
-        void print() {
-            for(int i=0; i<4; i++) {
-                Node node = list[i];
-                int count = 0;
-                while(node !=null) {
-                    count++;
-                    System.out.println("-----"+i+"-----" + node.num);
-                    node = node.link;
-                }
-                result.add(count);
-
-                if(count !=0) {
-                    sum -= count;
-                    sum = Math.abs(sum);
-                }
-            }
-            for(int a : result) {
-                System.out.println(a);
-            }
-            System.out.println("------------------");
-        }
-
-        int getSum() {
-            return sum;
-        }
-
-    }
-
+    
+    
+    //아..이거도 아닌데
+    
+    static int [][] arr;
+    static boolean [] visited;
+    static int count = 0;
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-        List<Integer> resultList = new ArrayList<>();
+        int cnt = 0;
+        while(cnt<wires.length) {
+            arr = new int[n+1][n+1];
+            visited = new boolean[n+1];
 
-        for(int i=0; i<wires.length-1; i++) {
-            NodeList nodeList = new NodeList();
-            for (int j = 0; j<wires.length; j++) {
-                if(j!=i) {
-                    nodeList.connect(wires[j][0], wires[j][1]);
+            for(int i=cnt; i<wires.length; i++) {
+                if(i!=cnt) {
+                    arr[wires[i][0]][wires[i][1]] = 1;
+                    arr[wires[i][1]][wires[i][0]] = 1;
                 }
             }
-            nodeList.print();
-            answer = Math.min(answer, nodeList.getSum());
+
+            for(int i=1; i<=n; i++) { // 정점의 개수
+                if(!visited[i]) {
+                    dfs(n,i,arr, visited);
+                    count++;
+                }
+            }
+            System.out.println(count);
+            count = 0;
+            cnt++;
         }
+
+
         return answer;
     }
+
+    private static void dfs(int n, int i, int[][] arr, boolean[] visited) {
+        visited[i] = true;
+        for(int j=1; j<=n; j++) {
+            if(!visited[j]&& arr[i][j]==1) {
+                dfs(n,j,arr,visited);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         int[][] wires = {{1,2},{2,3},{3,4}};
@@ -80,5 +54,11 @@ public class Solution {
         int n = 4;
         Solution solution = new Solution();
         System.out.println(solution.solution(n,wires));
+
+        /**
+         *
+         *
+         */
+
     }
 }
