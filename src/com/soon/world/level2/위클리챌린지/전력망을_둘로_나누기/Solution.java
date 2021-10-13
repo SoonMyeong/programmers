76a849ab16edd5ffabba265c7334c562c9cb2141
soonmyeong <smkwon@neonexsoft.com>
2021-10-13 23:03:11 +0900

    결국 다시 dfs 개념 익히고 다른분들 풀이 참고해서 해결 하긴했는데 매우 찝찝하다.. TODO. 연결리스트로도 해결 해보기, BFS로도 해결해보기

--- src/com/soon/world/level2/위클리챌린지/전력망을_둘로_나누기/Solution.java
+++ src/com/soon/world/level2/위클리챌린지/전력망을_둘로_나누기/Solution.java
@@ -1,35 +1,42 @@
 package com.soon.world.level2.위클리챌린지.전력망을_둘로_나누기;
 
 
+import java.util.Arrays;
+
 public class Solution {
-    
-    
-    //아..이거도 아닌데
-    
+
     static int [][] arr;
     static boolean [] visited;
     static int count = 0;
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         int cnt = 0;
+        arr = new int[n+1][n+1];
+        visited = new boolean[n+1];
+
+        for(int i=0; i<wires.length; i++) {
+            arr[wires[i][0]][wires[i][1]] = 1;
+            arr[wires[i][1]][wires[i][0]] = 1;
+        }
+
         while(cnt<wires.length) {
-            arr = new int[n+1][n+1];
-            visited = new boolean[n+1];
+            arr[wires[cnt][0]][wires[cnt][1]] = 0;
+            arr[wires[cnt][1]][wires[cnt][0]] = 0;
 
-            for(int i=cnt; i<wires.length; i++) {
-                if(i!=cnt) {
-                    arr[wires[i][0]][wires[i][1]] = 1;
-                    arr[wires[i][1]][wires[i][0]] = 1;
-                }
-            }
+            dfs(1,n);
 
-            for(int i=1; i<=n; i++) { // 정점의 개수
-                if(!visited[i]) {
-                    dfs(n,i,arr, visited);
-                    count++;
-                }
-            }
+            arr[wires[cnt][0]][wires[cnt][1]] = 1;
+            arr[wires[cnt][1]][wires[cnt][0]] = 1;
             System.out.println(count);
+
+            Arrays.fill(visited, false);
+
+            /**
+             * n- count 또 다른 송전탑 개수
+             * 이 두개의 송전탑 개수의 차
+             * n-count - count = n- 2*count
+             */
+            answer = Math.min(answer,Math.abs(n- (2*count)));
             count = 0;
             cnt++;
         }
@@ -38,20 +45,21 @@ public class Solution {
         return answer;
     }
 
-    private static void dfs(int n, int i, int[][] arr, boolean[] visited) {
+    private static void dfs(int i, int n) {
         visited[i] = true;
+        count++;
         for(int j=1; j<=n; j++) {
             if(!visited[j]&& arr[i][j]==1) {
-                dfs(n,j,arr,visited);
+                dfs(j,n);
             }
         }
     }
 
 
     public static void main(String[] args) {
-        int[][] wires = {{1,2},{2,3},{3,4}};
-//        int[][] wires = {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}};
-        int n = 4;
+//        int[][] wires = {{1,2},{2,3},{3,4}};
+        int[][] wires = {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}};
+        int n = 9;
         Solution solution = new Solution();
         System.out.println(solution.solution(n,wires));
 
